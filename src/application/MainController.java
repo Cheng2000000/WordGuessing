a9b86006062c63d58936bfbef6c8494989844cfb
Lang <lche262@aucklanduni.ac.nz>
2021-09-22 08:21:46 +1200

    Finish buidling up the scenes template of Modes and QuizScene

---
+++ src/application/MainController.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainController {
+
+	@FXML
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+
+	public void newGame(ActionEvent event) throws IOException {
+		root = FXMLLoader.load(getClass().getResource("Modes.fxml"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	public void quit(ActionEvent event) throws IOException {
+		Platform.exit();
+	}
+}
