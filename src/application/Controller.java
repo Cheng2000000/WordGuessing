614dec7cc4dcc5f17c80065b4b3ab7a4d0f8722d
Lang <lche262@aucklanduni.ac.nz>
2021-09-21 23:46:54 +1200

    Created a main scene for the word guessing game

---
+++ src/application/Controller.java
@@ -0,0 +1,16 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controller {
+	
+	@FXML
+	public void newGame(ActionEvent e) {
+		System.out.println("up");
+	}
+	
+	public void quit(ActionEvent e) {
+		System.out.println("up");
+	}
+}
